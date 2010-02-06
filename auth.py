bd75f8790533a1befec08176ed4852336bf4f3de
IIssNan <iissnan@gmail.com>
2010-02-06 17:07:28 +0800

    Initial commit
    已注释掉Google Ads

---
+++ auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# SECRET is SHA1 of your passphrase, you can use Python hashlib to generate it:
+#
+# Example using Python CLI:
+#
+# >>> import hashlib
+# >>> hashlib.sha1('secret').hexdigest()
+# 'e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4'
+
+SECRET = 'dfb3e7f49f5f2d3930f5425ad6c1855fca983afa'
