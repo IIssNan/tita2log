0696742e85171055e690e5213cff2f0aa553b3dc
ivanChoi <ivan@ivanChois-MacBook-Pro.local>
2010-09-26 03:33:48 +0800

    [Theme DoubleKS] All Sprited!

---
+++ static/shared/ckeditor/config.js
@@ -0,0 +1,23 @@
+/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	
+	config.toolbar = 'Qzone';
+	
+	    config.toolbar_Qzone =
+	    [
+	        ['Font','FontSize'],
+	        ['Bold','Italic','Underline','Strike'],
+	        ['TextColor','BGColor','-','Outdent','Indent','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','Maximize'],
+	        '/',
+	        ['Smiley','Image','Vedio','Flash','-','NumberedList','BulletedList','Blockquote'],
+	        ['Undo','Redo']
+	    ];
+};
